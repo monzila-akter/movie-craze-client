5c5867f4f73a84148b0514a619156798bf0ca492
monzila-akter <monzila01727@gmail.com>
2024-12-05 03:32:32 +0600

    added featured movies section

---
+++ src/components/FeaturedMovies.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+
+const FeaturedMovies = ({movie}) => {
+
+    const { _id, poster, title, genre, duration, releaseYear, rating } = movie;
+
+    return (
+        <div className="card bg-gray-800 text-white shadow-xl rounded-lg overflow-hidden">
+        <figure>
+          <img src={poster} alt={title} className="w-full h-60 object-cover" />
+        </figure>
+        <div className="card-body p-5">
+          <h2 className="card-title text-xl font-semibold mb-2">{title}</h2>
+          <p className="text-sm text-gray-400 mb-2"><span  className='text-white font-semibold'>Genre:</span>  {genre}</p>
+          <div className="flex justify-between text-sm text-gray-300">
+            <span>{duration} minutes</span>
+            <span>{releaseYear}</span>
+          </div>
+          <div className="mt-2 text-red-500 font-semibold"><span className='text-white font-semibold'>Rating:</span> {rating} / 5</div>
+          <div className="card-actions justify-center mt-4">
+            <Link to={`/movie/${_id}`} className="btn bg-transparent border-2 border-red-500 text-red-500 text-lg font-semibold hover:bg-transparent hover:text-white ">
+              See Details
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default FeaturedMovies;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../components/Banner";
-
-
-
+import FeaturedMovies from "../components/FeaturedMovies";
 
 const Home = () => {
+
+   const featuredMovies = useLoaderData();
+
     return (
         <div>
-          
             <Banner></Banner>
-           
+           <div className="w-11/12 mx-auto px-5 md:px-10 lg:px-14 mb-16">
+            <h2 className="text-4xl font-bold mb-10 text-red-500 text-center">Featured Movies</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    featuredMovies.map(movie => <FeaturedMovies key={movie._id} movie={movie}></FeaturedMovies>)
+                }
+            </div>
+           </div>
         </div>
     );
 };
