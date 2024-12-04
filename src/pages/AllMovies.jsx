1e496ebb26ff600788e42eaf50a477b1d36b2fb9
monzila-akter <monzila01727@gmail.com>
2024-12-05 03:03:56 +0600

    added allMoviesCard for all movies page

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import AllMovieCard from "../components/AllMovieCard";
 
 
 const AllMovies = () => {
@@ -6,11 +7,11 @@ const AllMovies = () => {
     const movies = useLoaderData();
 
     return (
-        <div>
-            <h2 className="text-3xl font-semibold text-gray-700 text-center mt-16">All Movies</h2>
-            <div>
+        <div className="w-11/12 mx-auto px-5 md:px-10 lg:px-14 mb-16">
+            <h2 className="text-3xl font-semibold text-gray-700 text-center mt-16 mb-10">All Movies</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
-                  movies.map(movie => )  
+                  movies.map(movie => <AllMovieCard key={movie._id} movie={movie}></AllMovieCard>)  
                 }
             </div>
         </div>
